5764370639caefc9213c05378375e7d285d9e26f
Yuliyala <yuliyalapenak@gmail.com>
2025-12-27 12:26:25 +0300

    Initial commit: ParkPin project setup

---
+++ ParkPin/ParkPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkPinApp.swift
+//  ParkPin
+//
+//  Created by Yuliya Lapenak on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
